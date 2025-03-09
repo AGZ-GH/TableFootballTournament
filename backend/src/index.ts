7c3e000140ee57d0b6c4ef0281eeb29412a64326
AGZ <goetzalexandre54@protonmail.com>
2025-03-09 21:14:18 +0100

    add typeorm to the project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,11 @@
+import "reflect-metadata"
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
