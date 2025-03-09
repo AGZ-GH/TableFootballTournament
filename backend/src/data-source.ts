8194d79e3b31baf770974c8545bc6c912a964220
AGZ <goetzalexandre54@protonmail.com>
2025-03-09 23:12:17 +0100

    add data source for orm

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,12 +1,19 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+
+import { Match } from "./entity/Match";
+import { Player } from "./entity/Player";
+import { Team } from "./entity/Team";
+import { Tournament } from "./entity/Tournament";
+
+const databaseName = process.env.DATABASENAME ?? "";
 
 export const AppDataSource = new DataSource({
-    
+    type: "sqlite",
+    database: databaseName,
+    entities: [Match,Player,Team,Tournament],
     synchronize: true,
     logging: false,
-    entities: [User],
     migrations: [],
     subscribers: [],
 })
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,25 +2,24 @@ import "reflect-metadata"
 import { DataSource } from "typeorm"
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import { Match } from "./entity/Match";
-import { Player } from "./entity/Player";
-import { Team } from "./entity/Team";
-import { Tournament } from "./entity/Tournament";
 
 dotenv.config();
 
-const AppDataSource = new DataSource({
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
-  database: "tableFootballTournament",
-  entities: [Match,Player,Team,Tournament],
-  synchronize: true,
-  logging: false,
-})
-  
+
+
 const app: Express = express();
 const port = process.env.PORT ?? 3000;
+
+async function connectDatabase() {
+  try {
+       
+    console.log("Database connected successfully!");
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+  }
+}
+
+connectDatabase();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
