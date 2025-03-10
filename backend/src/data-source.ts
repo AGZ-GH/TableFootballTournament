135b4a4f6ca03be460a3bf07a0afb41af6ca8f76
AGZ <goetzalexandre54@protonmail.com>
2025-03-10 16:33:07 +0100

    change db to mysql

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,17 +1,24 @@
 import "reflect-metadata"
+import dotenv from "dotenv";
 import { DataSource } from "typeorm"
-
 import { Match } from "./entity/Match.entity";
 import { Player } from "./entity/Player.entity";
 import { Team } from "./entity/Team.entity";
 import { Tournament } from "./entity/Tournament.entity";
 
-const databaseName = process.env.DATABASENAME ?? "";
+dotenv.config();
 
+const databaseName = process.env.DATABASE_NAME ?? "";
+const databasePort = process.env.DATABASE_PORT ?? 3306;
+const databaseHost = process.env.DATABASE_HOST ?? "";
+const databaseUsername = process.env.DATABASE_USERNAME ?? "lala";
+const databasePwd = process.env.DATABASE_PWD ?? ""
 export const AppDataSource = new DataSource({
-    type: "sqlite",
+    type: "mysql",
+    host: databaseHost,
     database: databaseName,
     entities: [Player,Team,Match,Tournament],
-    synchronize: true ,
-    logging: false, 
-})
+    username: databaseUsername,   
+    password: databasePwd,
+    synchronize: true,
+    })
